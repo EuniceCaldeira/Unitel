4a9e561df0a3f462002e4daa27463c2b7ea2d408
EuniceCaldeira <manuelaeunice262@gmail.com>
2022-10-14 16:53:19 +0100

    segundo commit

---
+++ app.j.js
@@ -0,0 +1,11 @@
+var numero1=10
+var numero2=5
+var numero3=7
+var multiplicação  (numero1*numero2*numero3);
+console.log("Resultado": * multiplicação);
+
+
+var numero4=5
+var numero5=2
+var valor (numero4/numero5);
+console.log("valor": / valor )
